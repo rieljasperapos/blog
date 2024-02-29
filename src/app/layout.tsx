c55b90a1035de76ad1bd9aebe7aed0ea6ec15537
Riel <jasper.apos@gmail.com>
2024-03-01 01:08:06 +0800

    style: header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={`h-full ${inter.className}`}>
+        <div className="flex flex-col h-full">
+          <Header />
+          <div className="flex flex-col flex-grow mt-12 mb-12">
+            {children}
+          </div>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
