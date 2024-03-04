8902b6ebac0e60ba601f43e86aff748a7000b526
Riel Apos <jasper.apos@gmail.com>
2024-03-04 09:00:22 +0800

    feat: api fetch testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+"use client"
 import RecentPost from "@/components/articles/recent-post";
 import BlogPosts from "@/components/articles/blog-posts";
+import { IBlog } from "@/types/blog-type";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [blogPost, setBlogPost] = useState<IBlog | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/blogs", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Error ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      if (data) {
+        setBlogPost(data);
+      }
+    })
+    .catch((err: Error) => {
+      console.log(`${err}`);
+    })
+  }, [])
+
   return (
     <div className="mx-6 lg:mx-24 flex flex-col justify-center items-center">
       <RecentPost />
