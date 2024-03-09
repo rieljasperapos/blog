7bd5d6868346ae310430cc1cb3688d6dfa2250db
Riel Apos <jasper.apos@gmail.com>
2024-03-09 18:38:15 +0800

    feat: back button that navigates to home page

--- src/components/blog-list.tsx
+++ src/components/blog-list.tsx
@@ -2,6 +2,8 @@
 import { BlogList } from "@/static/content";
 import { Content2 } from "@/static/content";
 import { useRouter } from "next/navigation";
+import { IoReturnUpBackOutline } from "react-icons/io5";
+import Link from "next/link";
 
 interface BlogProps {
   title: string;
@@ -15,12 +17,16 @@ const Blogs: React.FC<BlogProps> = ({ title }) => {
   return (
     <>
       <div className="cursor-pointer flex flex-col gap-4">
+        <Link href="/" className="flex items-center gap-4 hover:text-orange-500 hover:font-bold">
+          <IoReturnUpBackOutline />
+          <p>Go back</p>
+        </Link>
         <h1 className="font-bold text-lg">Blog Posts</h1>
         <ul>
           {Content2.map((blog, idx) => (
             <li 
               key={idx} 
-              className={`${blog.title === title.replaceAll("-", " ") ? "text-orange-500" : ""} mb-4 hover:text-orange-500 hover:underline`}
+              className={`${blog.title === title.replaceAll("-", " ") ? "text-orange-500" : ""} mb-2 hover:text-orange-500 hover:underline text-sm`}
               onClick={() => handleClick(blog.title)}
               >
                 {blog.title}
