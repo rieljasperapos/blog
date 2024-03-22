a70704022be67ca2936451e4532ee062eeb3bc9e
Riel Apos <jasper.apos@gmail.com>
2024-03-22 18:00:54 +0800

    feat: fetched blog titles from sanity

--- src/components/drawer-mobile.tsx
+++ src/components/drawer-mobile.tsx
@@ -11,14 +11,34 @@ import {
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Button } from "@/components/ui/button"
 import { IoHomeOutline } from "react-icons/io5";
-import { Content2 } from "@/static/content";
+import { fetchPosts } from "@/utils/sanity/fetch-posts";
 import { useRouter } from "next/navigation";
+import { Post } from "@/types/blog-type";
 
 const Drawer = () => {
+  const [blogPosts, setBlogPosts] = useState<Post[]>();
   const [currentURL, setCurrentURL] = useState('');
   const navigate = useRouter();
 
+  const handleClick = (title: string | undefined) => {
+    console.log(title);
+    if (title === "/") {
+      setCurrentURL('/');
+      navigate.push('/');
+    } else {
+      const newUrl = `/blogs/${title}`;
+      setCurrentURL(newUrl);
+      navigate.push(newUrl);
+    }
+  }
+
   useEffect(() => {
+    fetchPosts().then(posts => {
+      setBlogPosts(posts);
+    }).catch(error => {
+      console.error("Error:", error);
+    });
+
     setCurrentURL(window.location.pathname);
     const handlePopState = () => {
       setCurrentURL(window.location.pathname);
@@ -28,66 +48,50 @@ const Drawer = () => {
     return () => {
       window.removeEventListener('popstate', handlePopState);
     };
-  }, []);
-
-  const handleClick = (title: string) => {
-    const sanitizedTitle = title.trim().replaceAll(" ", "-");
-    if (title === "/") {
-      setCurrentURL('/');
-      navigate.push('/');
-    } else {
-      const newUrl = `/blogs/${sanitizedTitle}`;
-      setCurrentURL(newUrl);
-      navigate.push(newUrl);
-    }
-  }
-
-  console.log(currentURL);
+  }, [])
 
   return (
-    <>
     <div className="lg:hidden">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
-              <RxHamburgerMenu />
-            </Button>
-          </SheetTrigger>
-            <SheetContent side="left">
-              <SheetHeader className="mb-10">
-                <SheetClose asChild>
-                  <SheetTitle asChild>
-                    <div className="flex">
-                      <h1>riel blogs</h1>
-                    </div>
-                  </SheetTitle>
-                </SheetClose>
-              </SheetHeader>
-              <SheetClose asChild>
-                <div className="flex items-center gap-4 hover:text-orange-500 hover:font-bold mb-4" onClick={() => handleClick("/")}>
-                    <div className="flex items-center gap-2">
-                      <IoHomeOutline />
-                      <span>Home</span>
-                    </div>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon">
+            <RxHamburgerMenu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="mb-10">
+            <SheetClose asChild>
+              <SheetTitle asChild>
+                <div className="flex">
+                  <h1>riel blogs</h1>
                 </div>
-              </SheetClose>
-              <hr></hr>
-              <div className="flex flex-col gap-4 mt-4">
-                <h1 className="font-medium">Articles</h1>
-              {Content2.map((blog) => (
-                <div key={blog.id} className="hover:text-orange-500 hover:underline hover:font-medium transition duration-300">
-                  <SheetClose asChild>
-                  <div className="flex items-center gap-4" onClick={() => handleClick(blog.title)}>
-                    <p className={`text-sm ${`/blogs/${blog.title.replaceAll(" ", "-")}` === currentURL ? `text-orange-500` : '' }`}>{blog.title}</p>
-                  </div>
-                  </SheetClose>
-                </div>
-              ))}
+              </SheetTitle>
+            </SheetClose>
+          </SheetHeader>
+          <SheetClose asChild>
+            <div className="flex items-center gap-4 hover:text-orange-500 hover:font-bold mb-4" onClick={() => handleClick("/")}>
+              <div className="flex items-center gap-2">
+                <IoHomeOutline />
+                <span>Home</span>
               </div>
-            </SheetContent>
-        </Sheet>
+            </div>
+          </SheetClose>
+          <hr></hr>
+          <div className="flex flex-col gap-4 mt-4">
+            <h1 className="font-medium">Articles</h1>
+            {blogPosts?.map((blog, idx) => (
+              <div key={idx} className="hover:text-orange-500 hover:underline hover:font-medium transition duration-300">
+                <SheetClose asChild>
+                  <div className="flex items-center gap-4" onClick={() => handleClick(blog.currentSlug)}>
+                    <p className={`text-sm ${`/blogs/${blog.currentSlug}` === currentURL ? `text-orange-500` : ``}`}>{blog.title}</p>
+                  </div>
+                </SheetClose>
+              </div>
+            ))}
+          </div>
+        </SheetContent>
+      </Sheet>
     </div>
-    </>
   )
 }
 
