1d0128d8989a1d8f2385d44424dacb819e6ad890
Riel Apos <jasper.apos@gmail.com>
2024-03-22 17:57:49 +0800

    chore: removed unused codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,7 @@
-"use client"
 import RecentPost from "@/components/articles/recent-post";
 import BlogPosts from "@/components/articles/blog-posts";
-import { IBlog } from "@/types/blog-type";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [blogPost, setBlogPost] = useState<IBlog | null>(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8080/blogs", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(`Error ${res.status}`);
-      }
-      return res.json();
-    })
-    .then((data) => {
-      if (data) {
-        setBlogPost(data);
-      }
-    })
-    .catch((err: Error) => {
-      console.log(`${err}`);
-    })
-  }, [])
-
   return (
     <div className="mx-6 lg:mx-24 flex flex-col justify-center items-center">
       <RecentPost />
@@ -39,3 +11,5 @@ export default function Home() {
     </div>
   );
 }
+
+export const dynamic = "force-dynamic";
