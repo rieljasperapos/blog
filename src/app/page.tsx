66be8ec3e25096c15a687750342c37b2131a9e9e
jasper <jasper.apos@gmail.com>
2025-02-24 01:03:27 +0800

    changed padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import BlogPosts from "@/components/articles/blog-posts";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center mx-auto max-w-2xl p-2 min-h-screen">
+    <div className="flex flex-col justify-center items-center mx-auto max-w-2xl p-3.5 min-h-screen">
       <RecentPost />
       <div className="border-t mt-16 lg:mt-24">
         <BlogPosts />
